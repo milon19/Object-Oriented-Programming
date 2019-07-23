3712624ccb4b1382466a23f8d911f98f8c66f112
Milon Mahato <33964827+milon19@users.noreply.github.com>
2019-07-23 22:06:19 +0600

    Create Car.java

---
+++ Object & Class/Car.java
@@ -0,0 +1,18 @@
+//Car is class.
+
+public class Car {
+    public String name;
+    public String color;
+    public int sum;
+
+ 
+    public void Car()
+    {
+        System.out.println(this.name + " is started");
+    }
+ 
+    public void start()
+    {
+        System.out.println(this.name + " is started");
+    }
+}
