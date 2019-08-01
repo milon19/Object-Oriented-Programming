e079859451650debb479893b1cf32f3db6d46ec7
Milon Mahato <33964827+milon19@users.noreply.github.com>
2019-08-01 23:17:13 +0600

    Create Teacher.java

---
+++ 8. Association/Bidirectional/Teacher.java
@@ -0,0 +1,40 @@
+package Bidirectional;
+
+import java.util.List;
+
+public class Teacher {
+	
+	int ID;
+	String Name;
+	String Email;
+	String Contact;
+	String Designation;
+	
+	public Teacher(int ID, String Name, String Email)
+	{
+		this.ID = ID;
+		this.Name = Name;
+		this.Email = Email;
+	}
+	void Registration()
+	{
+		
+	}
+	public void Teach(List<Student> Students)
+	{
+		System.out.println(this.Name+" is teaching the following Students: ");
+		for(Student aStudent : Students)
+		{
+			System.out.println(aStudent.Name);
+		}
+	}
+	public void Supervision(List<Student> Students)
+	{
+		System.out.println(this.Name+" is supervising the following Students: ");
+		for(Student aStudent : Students)
+		{
+			System.out.println(aStudent.Name);
+		}
+	}
+
+}
