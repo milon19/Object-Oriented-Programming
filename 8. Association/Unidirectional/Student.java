b6c22875092be92324a4aa8cd985c055a6b9d101
Milon Mahato <33964827+milon19@users.noreply.github.com>
2019-08-01 21:53:35 +0600

    Create Student.java

---
+++ 8. Association/Unidirectional/Student.java
@@ -0,0 +1,19 @@
+package Unidirectional;
+
+public class Student {
+	int ID;
+	String Name;
+	String Email;
+	String Contact;
+	
+	public Student(int ID, String Name, String Email)
+	{
+		this.ID = ID;
+		this.Name = Name;
+		this.Email = Email;
+	}
+	void Registration()
+	{
+		
+	}
+}
