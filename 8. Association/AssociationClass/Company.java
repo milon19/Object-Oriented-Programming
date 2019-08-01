8600f7897e786bebd8c2da75fefbbb2e48b8e7a5
Milon Mahato <33964827+milon19@users.noreply.github.com>
2019-08-02 00:55:15 +0600

    Create Company.java

---
+++ 8. Association/AssociationClass/Company.java
@@ -0,0 +1,15 @@
+package AssociationClass;
+
+public class Company {
+	public String ID;
+	public String Name;
+	public String Address;
+	
+	public Company(String ID, String Name, String ad)
+	{
+		this.ID = ID;
+		this.Name = Name;
+		this.Address = ad;
+	}
+
+}
